c996838cb18a2431dec6bca298b6b740e9dcc5f4
zi0w <zzz13588@naver.com>
2024-12-18 18:38:00 +0900

    feat: Add Metadata to ratation page

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Head from "next/head";
 
 import { Champion } from "@/lib/types/Champion";
 import { getChampionRotation } from "@/lib/utils/rotationApi";
@@ -31,42 +32,64 @@ export default function RotationPage() {
   const { allPlayers, newPlayers } = data;
 
   return (
-    <section className="grid justify-items-center min-h-screen py-8 pb-20 m-auto max-w-custom container">
-      <article className="flex flex-col gap-16 p-4">
-        <div>
-          <div className="pb-10">
-            <h2 className="font-bold text-3xl">금주 플레이어 로테이션 확인</h2>
-            <p className="font-light">
-              Riot Games API를 활용하여 금주 플레이어 로테이션 정보를
-              제공합니다.
-            </p>
+    <>
+      <Head>
+        <title>금주 플레이어 로테이션 확인</title>
+        <meta
+          name="description"
+          content="Riot Games API를 활용하여 금주 플레이어 로테이션 정보를 제공합니다."
+        />
+        <meta name="og:title" content="금주 플레이어 로테이션 확인" />
+        <meta
+          name="og:description"
+          content="Riot Games API를 활용하여 금주 플레이어 로테이션 정보를 제공합니다."
+        />
+        <meta
+          name="og:images"
+          content="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_1.jpg"
+        />
+        <meta name="og:url" content="http://localhost:3000/champions" />
+      </Head>
+
+      <section className="grid justify-items-center min-h-screen py-8 pb-20 m-auto max-w-custom container">
+        <article className="flex flex-col gap-16 p-4">
+          <div>
+            <div className="pb-10">
+              <h2 className="font-bold text-3xl">
+                금주 플레이어 로테이션 확인
+              </h2>
+              <p className="font-light">
+                Riot Games API를 활용하여 금주 플레이어 로테이션 정보를
+                제공합니다.
+              </p>
+            </div>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+              {allPlayers.map((champion) => (
+                <ChampionCard key={champion.id} champion={champion} />
+              ))}
+            </div>
           </div>
 
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-            {allPlayers.map((champion) => (
-              <ChampionCard key={champion.id} champion={champion} />
-            ))}
-          </div>
-        </div>
+          <div>
+            <div className="pb-10">
+              <h2 className="font-bold text-3xl">
+                금주 신규 플레이어 로테이션 확인
+              </h2>
+              <p className="font-light">
+                Riot Games API를 활용하여 금주 신규 플레이어 로테이션 정보를
+                제공합니다.
+              </p>
+            </div>
 
-        <div>
-          <div className="pb-10">
-            <h2 className="font-bold text-3xl">
-              금주 신규 플레이어 로테이션 확인
-            </h2>
-            <p className="font-light">
-              Riot Games API를 활용하여 금주 신규 플레이어 로테이션 정보를
-              제공합니다.
-            </p>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+              {newPlayers.map((champion) => (
+                <ChampionCard key={champion.id} champion={champion} />
+              ))}
+            </div>
           </div>
-
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-            {newPlayers.map((champion) => (
-              <ChampionCard key={champion.id} champion={champion} />
-            ))}
-          </div>
-        </div>
-      </article>
-    </section>
+        </article>
+      </section>
+    </>
   );
 }
--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import DarkMode from "./DarkMode";
 
 const Nav = () => {
